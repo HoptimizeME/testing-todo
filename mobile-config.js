dc2cc1da276168cfa4399a6707e6979b9a91b11b
JF-Chandi <JF-Chandi@users.noreply.github.com>
2015-09-09 15:29:04 -0400

    Update mobile-config.js

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   name: 'Todos',
-  description: 'A simple todo list app built in Meteor.',
+  description: 'A simple todo list app built in Meteor ***CHANGEMENT DANS MASTER***.',
   author: 'Percolate Studio Team',
   email: 'us@percolatestudio.com',
   website: 'http://percolatestudio.com',
